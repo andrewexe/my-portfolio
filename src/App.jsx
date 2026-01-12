4333bae82884656edfa411b1c64258d1fc05594f
Andrew Huang <andrew03huang@gmail.com>
2026-01-12 18:17:33 -0500

    updated experience

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,15 @@ const CONTACT = [
 const EXPERIENCE = [
   {
       org: "Software Engineer Intern",
+      title: "Pinterest",
+      range: "May '26 – Aug '26",
+      type: "Internship",
+      bullets: [
+        "Incoming Summer 2026",
+      ],
+  },
+{
+      org: "Software Engineer Intern",
       title: "Sycomp",
       range: "Jan '26 – May '26",
       type: "Internship",
